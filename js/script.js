7cbf8a1f58b023cadbfa9598c463bb4d63006cfd
Serhii Merva <aquila.moro@gmail.com>
2022-05-16 20:57:03 +0300

    added smooth transition

--- js/script.js
+++ js/script.js
@@ -9,3 +9,37 @@ if (iconMenu) {
     menuBody.classList.toggle("_active");
   });
 }
+
+//scroll by click
+
+const menuLinks = document.querySelectorAll(".menu_link[data-goto]");
+if (menuLinks.length > 0) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        pageYOffset -
+        document.querySelector(".navigation").offsetHeight;
+
+      if (iconMenu.classList.contains("_active")) {
+        iconMenu.classList.remove("_active");
+        document.body.classList.remove("_lock");
+        menuBody.classList.remove("_active");
+      }
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth",
+      });
+      e.preventDefault();
+    }
+  }
+}
